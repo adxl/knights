8a8e210e090b6a3735e4c79c36e380ac96cef750
Adel Senhadji <adelsenhadjii@gmail.com>
2020-04-22 17:38:53 +0200

    fix board initialization

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -14,11 +14,11 @@ export default class Board extends Component {
 	
 	sketch = (p) => {
 
-		const canvasSize = 500;
-
+		const canvasSize = 600;
+		
 		p.setup = () => {
 			p.createCanvas(canvasSize,canvasSize);
-			renderBoard();
+			initBoard();
 		};
 
 		p.draw = () => {
@@ -27,28 +27,40 @@ export default class Board extends Component {
 			p.noLoop();
 		};
 
-		function renderBoard() {
-			
+		function initBoard() {
+			const size = canvasSize / 3;
+			const boardIndexes = [
+				[1,2,3],
+				[4,5,6],
+				[7,8,9]
+			];
+
+			for (let i = 0; i < 3; i++) {
+				for (let j = 0; j < 3; j++) {
+					if (boardIndexes[i][j] % 2 !== 0) 
+						p.fill(50);
+					else 
+						p.fill(245);
+						
+					p.stroke(50);
+					p.strokeWeight(2);
+					p.rect(size * i + 5,size * j + 5,size - 5,size - 5);
+				}
+			}
+
+		}
+		
+		function resetBoard() {
 			let board = [];
 				
 			board[0] = [1,0,1];		
 			board[1] = [0,0,0];		
-			board[2] = [1,0,1];		
-		
-			const size = canvasSize / 3;
+			board[2] = [1,0,1];	
+			
+		}
 
-			for (let i = 0; i < 3; i++) {
-				for (let j = 0; j < 3; j++) {
-					if (board[i][j] === 1) 
-						p.fill(50);
-					else 
-						p.fill(245);
-					p.stroke(20);
-					p.strokeWeight(2);
-					p.rect(size * i + 5,size * j + 5,size - 5,size - 5);
-
-				}
-			}
+		function renderBoard() {
+			
 		}
 	}
 
